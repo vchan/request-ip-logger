16f1a59aa98dcc845a8ba83f27818ddb737d8aff
Vincent Chan <vincent.chan@radius.com>
2017-02-21 12:49:59 -0800

    Added command line arguments for luminati

--- request_ip_logger/luminati.py
+++ request_ip_logger/luminati.py
@@ -10,9 +10,26 @@ PROXIES = {
 }
 
 
+def make_requests(num_requests, concurrent_requests):
+    """Make `num_requests` divided over `concurrent_requests`"""
+    num_iterations = num_requests // concurrent_requests
+    for i in range(num_iterations):
+        yield request_ip(proxies=PROXIES, concurrent_requests=concurrent_requests)
+    remainder = num_requests - (num_iterations * concurrent_requests)
+    if remainder:
+        yield request_ip(proxies=PROXIES, concurrent_requests=remainder)
+
+
 if __name__ == '__main__':
-    # Count occurrences of IPs used by Luminati in 100 requests
+    """Count occurrences of IPs used by Luminati"""
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('num_requests', help='total number of requests', type=int)
+    parser.add_argument('concurrent_requests', help='number of requests to make concurrently', type=int)
+    args = parser.parse_args()
+
     ip_counts = Counter()
-    for i in range(10):
-        ip_counts.update(request_ip(proxies=PROXIES, concurrent_requests=10))
+    for r in make_requests(args.num_requests, args.concurrent_requests):
+        ip_counts.update(r)
     pprint.pprint(ip_counts)
